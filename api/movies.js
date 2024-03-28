81906c51e9fcd63ef1009c164f5e82c2f6bdf2d5
Your Name <you@example.com>
2024-03-28 15:48:46 +0700

    fc

--- api/movies.js
+++ api/movies.js
@@ -14,4 +14,52 @@ router.get("/get-all-movie", async (req, res) => {
   }
 });
 
+// get top 10 movies action
+router.get("/get-top-10-movie-action", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool.request().query(`SELECT * FROM movies where category='Action'`);
+    const movies = result.recordset;
+    res.json({ data: movies, success: true });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get top 10 movies hành động
+router.get("/get-top-10-movie-hanhdong", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool.request().query(`SELECT * FROM movies where category='Action'`);
+    const movies = result.recordset;
+    res.json({ data: movies, success: true });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get top 10 movies võ thuật
+router.get("/get-top-10-movie-vothuat", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool.request().query(`SELECT * FROM movies where category='Action'`);
+    const movies = result.recordset;
+    res.json({ data: movies, success: true });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get top 10 movies kinh dị
+router.get("/get-top-10-movie-kinhdi", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool.request().query(`SELECT * FROM movies where category='Action'`);
+    const movies = result.recordset;
+    res.json({ data: movies, success: true });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
