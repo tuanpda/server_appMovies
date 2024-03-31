3370264a3518fa81c7e91edf092a24a99bac5273
Your Name <you@example.com>
2024-03-31 09:30:38 +0700

    git from home

--- api/movies.js
+++ api/movies.js
@@ -107,6 +107,21 @@ router.get("/get-all-movie-with-cat", async (req, res) => {
   }
 });
 
+// get top 10 with categories
+router.get("/get-top-10-movie-with-cat", async (req, res) => {
+  try {
+    await pool.connect();
+    const result = await pool
+      .request()
+      .input("category", req.query.category)
+      .query(`select top 10 * from movies where category = @category`);
+    const movies = result.recordset;
+    res.json({ data: movies, success: true });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // get top 10 movies newler
 router.get("/get-top-10-movie-new-film", async (req, res) => {
   try {
@@ -134,7 +149,6 @@ router.get("/get-top-10-movie-single-film", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
 
 // get top 10 movies hành động
 router.get("/get-top-10-movie-hanhdong", async (req, res) => {
