777649dfc1954714a9e890f08d7ad70448420c5d
tuanpda <tuanpda.bhxh@gmail.com>
2024-06-13 10:29:48 +0700

    cmit by tuanpda

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require("morgan");
 const verifyToken = require('./services/verify-token');
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 dotenv.config();
 
-// Middleware để giải quyết CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-  });
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+};
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -26,7 +25,8 @@ app.use('/api/movies', require('./api/movies'));
 
 
 // Middleware xác thực chỉ áp dụng cho các endpoint cần được bảo vệ
-app.use(['/', '/api/users', '/api/danhmucs', '/api/nodemailer'], verifyToken);
+// app.use(['/', '/api/users', '/api/danhmucs', '/api/nodemailer'], verifyToken);
+app.use(['/api/users', '/api/danhmucs', '/api/nodemailer'], verifyToken);
 
 app.get('/', (req, res) => {
     res.send('<h1>🤖 API SQLSERVER from NODEJS - TEST</h1>');
@@ -36,6 +36,11 @@ app.get('/', (req, res) => {
 app.use('/api/users', require('./api/users'));
 app.use('/api/tochucdvt', require('./api/tochucdvt'));
 
-app.listen(process.env.PORT, () => {
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server started running on ${process.env.PORT} for ${process.env.NODE_ENV}`);
+// }); bỏ phần này vì sẽ dùng https
+
+// Tạo máy chủ HTTPS
+https.createServer(options, app).listen(process.env.PORT, () => {
     console.log(`Server started running on ${process.env.PORT} for ${process.env.NODE_ENV}`);
 });
